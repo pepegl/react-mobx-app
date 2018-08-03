6a7047419f05bb0170d6a3213cb97f91e5f5285e
Petrica Petrea <petrica.petrea@outlook.com>
2018-08-03 15:15:21 +0300

    add css

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import Header from './Header';
 import React from 'react';
-import { Switch, Route, withRouter } from 'react-router-dom';
-import { inject, observer } from 'mobx-react';
+import {Route, Switch, withRouter} from 'react-router-dom';
+import {inject, observer} from 'mobx-react';
 import PrivateRoute from './PrivateRoute';
 
 import Article from './Article';
@@ -11,45 +11,46 @@ import Login from './Login';
 import Profile from './Profile';
 import Register from './Register';
 import Settings from './Settings';
+import './App.css';
 
 @inject('userStore', 'commonStore')
 @withRouter
 @observer
 export default class App extends React.Component {
 
-  componentWillMount() {
-    if (!this.props.commonStore.token) {
-      this.props.commonStore.setAppLoaded();
+    componentWillMount() {
+        if (!this.props.commonStore.token) {
+            this.props.commonStore.setAppLoaded();
+        }
     }
-  }
 
-  componentDidMount() {
-    if (this.props.commonStore.token) {
-      this.props.userStore.pullUser()
-        .finally(() => this.props.commonStore.setAppLoaded());
+    componentDidMount() {
+        if (this.props.commonStore.token) {
+            this.props.userStore.pullUser()
+                .finally(() => this.props.commonStore.setAppLoaded());
+        }
     }
-  }
 
-  render() {
-    if (this.props.commonStore.appLoaded) {
-      return (
-        <div>
-          <Header />
-          <Switch>
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/editor/:slug?" component={Editor} />
-            <Route path="/article/:id" component={Article} />
-            <PrivateRoute path="/settings" component={Settings} />
-            <Route path="/@:username" component={Profile} />
-            <Route path="/@:username/favorites" component={Profile} />
-            <Route path="/" component={Home} />
-          </Switch>
-        </div>
-      );
+    render() {
+        if (this.props.commonStore.appLoaded) {
+            return (
+                <div>
+                    <Header/>
+                    <Switch>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/register" component={Register}/>
+                        <Route path="/editor/:slug?" component={Editor}/>
+                        <Route path="/article/:id" component={Article}/>
+                        <PrivateRoute path="/settings" component={Settings}/>
+                        <Route path="/@:username" component={Profile}/>
+                        <Route path="/@:username/favorites" component={Profile}/>
+                        <Route path="/" component={Home}/>
+                    </Switch>
+                </div>
+            );
+        }
+        return (
+            <Header/>
+        );
     }
-    return (
-      <Header />
-    );
-  }
 }
